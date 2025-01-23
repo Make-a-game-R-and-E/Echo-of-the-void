06ea1e5c82e34014c39a9d77c136d8dbd824ccf0
rony.levy1 <roniki04@gmail.com>
2025-01-24 01:05:00 +0200

    doors

--- Assets/Scripts/PowerCells.cs
+++ Assets/Scripts/PowerCells.cs
@@ -1,14 +1,36 @@
+// using UnityEngine;
+
+// public class PowerCell : MonoBehaviour
+// {
+//     [SerializeField] string cellColor;
+//     void OnTriggerEnter(Collider other)
+//     {
+//         if (other.CompareTag("Player")) // If the player collides with the power cell
+//         {
+//             Debug.Log($"Collected {cellColor} power cell!");
+//             Destroy(gameObject); // Destroy the power cell
+//         }
+//     }
+// }
+
+
 using UnityEngine;
 
 public class PowerCell : MonoBehaviour
 {
-    [SerializeField] string cellColor;
+    [SerializeField] string cellColor; // הצבע של הכדור
+
+    public string GetCellColor()
+    {
+        return cellColor;
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) // If the player collides with the power cell
+        if (other.CompareTag("Player")) // אם השחקן פוגע בכדור
         {
             Debug.Log($"Collected {cellColor} power cell!");
-            Destroy(gameObject); // Destroy the power cell
+            Destroy(gameObject); // השמדת הכדור
         }
     }
 }
